4acb5a7132da89fe4f38a9fd51eca8cf9aab2f50
Dyson Thomas <dysonthomas112@gmail.com>
2022-02-24 13:53:02 +0530

    initial stage

---
+++ lib/screeens/cityscreens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CityScreen extends StatefulWidget {
+  const CityScreen({Key? key}) : super(key: key);
+
+  @override
+  _CityScreenState createState() => _CityScreenState();
+}
+
+class _CityScreenState extends State<CityScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screeens/loadingpage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({Key? key}) : super(key: key);
+
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Weather Bot"),
+      ),
+      body: ElevatedButton(
+        onPressed: () {},
+        child: Text('Get Location'),
+      ),
+    );
+  }
+}
